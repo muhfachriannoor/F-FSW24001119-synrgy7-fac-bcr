073a87fffaeafdbe3227ac58b2862a055ad7aa02
muhfachriannoor <muhammadfachriannoor@gmail.com>
2024-04-19 21:28:00 +0800

    fix: looping filter

--- scripts/filterCarByAvailability.js
+++ scripts/filterCarByAvailability.js
@@ -6,11 +6,14 @@ function filterCarByAvailability(cars) {
   const result = [];
 
   // Tulis code-mu disini
-  cars.forEach((car) => {
-    if (car.available) {
-      result.push(car);
+  Array.prototype.filterAvaibleTrue = function () {
+    for (let i = 0; i < this.length; i++) {
+      if (this[i].available === true) {
+        result.push(this[i]);
+      }
     }
-  });
+  };
+  cars.filterAvaibleTrue();
 
   // Rubah code ini dengan array hasil filter berdasarkan availablity
   return result;
