5cb1c962a66c78978464d61876880bb5301b84bb
muhfachriannoor <muhammadfachriannoor@gmail.com>
2024-04-19 13:40:42 +0800

    First init commit

---
+++ scripts/sortCarByYearAscendingly.js
@@ -0,0 +1,27 @@
+function sortCarByYearAscendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+
+  // Tulis code-mu
+  Array.prototype.bubbleSort = function () {
+    let n = this.length;
+    for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        if (this[j].year > this[j + 1].year) {
+          let temp = this[j];
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+  };
+
+  result.bubbleSort();
+
+  // Rubah code ini dengan array hasil sorting secara ascending
+  return result;
+}
